e509cdc6f20a487c7b9acc18bee6d941a1fe0504
Antonio Solis <antonio.solis1616@gmail.com>
2022-04-16 02:33:08 -0400

    Fixed bugs and updated front end with new buttons and new designs

--- bugs/forms.py
+++ bugs/forms.py
@@ -1,6 +1,9 @@
 from django.forms import ModelForm
 from .models import Ticket, Update
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+#Antonio: form to register a new user into the system.
 class TicketForm(ModelForm):
     class Meta:
         model = Ticket
@@ -10,3 +13,22 @@ class UpdateForm(ModelForm):
     class Meta:
         model = Update
         fields = '__all__'
+
+class RegisterUser(UserCreationForm):
+	email = forms.EmailField(widget = forms.EmailInput(attrs = {'class':'form-control'}))
+	first_name = forms.CharField(max_length = 40,widget = forms.TextInput(attrs = {'class':'form-control'}))
+	last_name = forms.CharField(max_length = 40,widget = forms.TextInput(attrs = {'class':'form-control'}))
+	address = forms.CharField(max_length = 150,widget = forms.TextInput(attrs = {'class':'form-control'}))
+	position = forms.CharField(max_length = 50,widget = forms.TextInput(attrs = {'class':'form-control'}))
+	department = forms.CharField(max_length = 50,widget = forms.TextInput(attrs = {'class':'form-control'}))
+	company = forms.CharField(max_length = 50,widget = forms.TextInput(attrs = {'class':'form-control'}))
+	class Meta:
+		model = User
+		fields = ('username', 'first_name', 'last_name','email','password1','password2','address','company','position','department')
+
+	def __init__(self,*args,**kwargs):
+		super(RegisterUser,self).__init__(*args,**kwargs)
+
+		self.fields['username'].widget.attrs['class'] = 'form-control'
+		self.fields['password1'].widget.attrs['class'] = 'form-control'
+		self.fields['password2'].widget.attrs['class'] = 'form-control'
--- bugs/urls.py
+++ bugs/urls.py
@@ -10,13 +10,15 @@ urlpatterns = [
     path('main/',views.main,name = "main"),
     path('login/',views.login),
     path('aboutNotLogged/', views.aboutNotLogged),
-    path('homePage/',views.homePage),
+    path('homePage/',views.homePage, name ="homePage"),
     path('createTicket/',views.createTicket),
     path('updateTicket/',views.updateTicket),
     path('deleteTicket/',views.deleteTicket),
     path('', views.home, name='home'),
     path('tickets/', views.tickets),
     path('about/', views.about),
-   
+    path('login_user', views.login_user, name = "login"),
+    path('logout_user',views.logout_user, name = 'logout'),
+    path('register_user',views.register_user, name ='register_user')
 
 ]
